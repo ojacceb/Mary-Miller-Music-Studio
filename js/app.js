5317e540b6c63c9b2a515276b5a1bbf2b664347b
Becca Taylor <beccat859@gmail.com>
2017-02-28 19:43:11 -0500

    Flashcard App: Added js folder and 3 flash card images

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//1. create array that contains questions and answers
+//2. display random question
+//3. display 2 buttons: "Check" and "Next" (HTML)
+//4. "Check" button reveals answer
+//5. "Next" button reveals next question
